d45d8e269b9579f878964272e1d3234bb86328e5
Raycu <46801977+Raycu@users.noreply.github.com>
2019-01-17 21:56:27 -0500

    Update main.c
    Comment

--- main.c
+++ main.c
@@ -15,6 +15,6 @@ int main(void){
 }
 
 void printRoom(ROOM *room){
-    printf("%s\n", room->name);
+    printf("%s\n", room->name); /* How the fuck does the %s thing work? I understand \n but not the % operator when printing */
     printf("%s\n", room->desc);
 }
